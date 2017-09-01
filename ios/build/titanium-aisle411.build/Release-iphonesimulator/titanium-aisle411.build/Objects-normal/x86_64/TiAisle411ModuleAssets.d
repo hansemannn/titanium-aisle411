8a337d661e8a7849bf53df5bf5c9dad079805b5c
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-09-01 17:16:14 +0200

    Remove build directory

--- ios/build/titanium-aisle411.build/Release-iphonesimulator/titanium-aisle411.build/Objects-normal/x86_64/TiAisle411ModuleAssets.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/hknoechel/Documents/appcelerator_modules/titanium-aisle411/TiAisle411_Prefix.pch \
-  /Users/hknoechel/Documents/appcelerator_modules/titanium-aisle411/Classes/TiAisle411ModuleAssets.m \
-  /Users/hknoechel/Documents/appcelerator_modules/titanium-aisle411/Classes/TiAisle411ModuleAssets.h
